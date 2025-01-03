0e69302bcf3d8efbf1b5d300893f805907ab5432
abounaime <a.bounaime@gmail.com>
2025-01-03 05:36:11 +0100

    # Refactoring AuthenticationRequest

--- src/main/java/com/example/experttrader/service/IgAuthService.java
+++ src/main/java/com/example/experttrader/service/IgAuthService.java
@@ -2,48 +2,61 @@ package com.example.experttrader.service;
 
 import com.example.experttrader.config.IgApiProperties;
 import com.example.experttrader.dto.AuthenticationRequest;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.function.Function;
+
 @Service
 public class IgAuthService {
-    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final static Logger log = LoggerFactory.getLogger(IgAuthService.class);
     private final IgApiProperties igApiProperties;
     private final WebClient webClient;
+    
     public IgAuthService(IgApiProperties igApiProperties,
                          WebClient.Builder webClientBuilder) {
         this.igApiProperties = igApiProperties;
-        this.webClient = webClientBuilder.baseUrl(igApiProperties.getBaseUrl()).build();
+        this.webClient = webClientBuilder
+                .baseUrl(igApiProperties.getBaseUrl())
+                .build();
     }
 
     public Mono<String> authenticate(){
-        AuthenticationRequest request = new AuthenticationRequest(igApiProperties.getUsername(),
-                igApiProperties.getPassword());
+        var request = createAuthenticationRequest();
 
         return webClient.post()
                 .uri("/session")
-                .header(HttpHeaders.CONTENT_TYPE, "application/json")
-                .header("X-IG-API-KEY", igApiProperties.getKey())
+                .headers(this::setHeaders)
                 .bodyValue(request)
                 .retrieve()
-                .onStatus(
-                        status -> status.is4xxClientError() ||
-                        status.is5xxServerError(),
-                        response ->
-                                Mono.error(
-                                        new RuntimeException("Authentication failed " +
-                                                "with status: "
-                                                + response.statusCode()))
-                )
+                .onStatus(statusCode -> statusCode.is5xxServerError()||
+                        statusCode.is4xxClientError(),
+                        clientResponse -> Mono.error(
+                                new RuntimeException("Authentication Failed "+
+                                        "with status "+clientResponse.statusCode())
+                        ))
                 .toEntity(String.class)
-                .map(response -> extractSecurityToken(
-                        response.getHeaders().getFirst("X-SECURITY-TOKEN")));
+                .map(objectResponseEntity ->
+                        extractSecurityToken(objectResponseEntity.getHeaders()
+                                .getFirst("X-SECURITY-TOKEN")));
+    }
+
+
+
+    private void setHeaders(HttpHeaders headers) {
+        headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
+        headers.set("X-IG-API-KEY", igApiProperties.getKey());
+    }
+
+    private AuthenticationRequest createAuthenticationRequest() {
+        return new AuthenticationRequest(igApiProperties.getUsername(),
+                igApiProperties.getPassword());
     }
 
     private String extractSecurityToken(String token) {
