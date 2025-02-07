e0d291af18c45546e883de9cb98f30bc17f4b0b7
abounaime <a.bounaime@gmail.com>
2025-02-07 14:29:48 +0100

    # Add Redis for tokens access cashing

--- src/main/java/com/example/experttrader/config/RedisConfig.java
+++ src/main/java/com/example/experttrader/config/RedisConfig.java
@@ -3,7 +3,6 @@ package com.example.experttrader.config;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
