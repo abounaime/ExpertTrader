933fc24730f6a51c9219181ff6735934fcd40afe
abounaime <a.bounaime@gmail.com>
2025-02-07 14:26:59 +0100

    # Add Redis for tokens access cashing

---
+++ src/main/java/com/example/experttrader/service/TokenStorageService.java
@@ -0,0 +1,31 @@
+package com.example.experttrader.service;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class TokenStorageService {
+    private final StringRedisTemplate redisTemplate;
+    private static final String CST_KEY = "IG_CST";
+    private static final String SECURITY_TOKEN_KEY = "IG_SECURITY_TOKEN";
+
+    public TokenStorageService(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+    public void storeTokens(String cst, String securityToken){
+        redisTemplate.opsForValue().set(CST_KEY, cst, Duration.ofHours(1));
+        redisTemplate.opsForValue().set(SECURITY_TOKEN_KEY, securityToken, Duration.ofHours(1));
+    }
+    public String getCstKey(){
+        return redisTemplate.opsForValue().get(CST_KEY);
+    }
+    public String getSecurityTokenKey(){
+        return redisTemplate.opsForValue().get(SECURITY_TOKEN_KEY);
+    }
+    public boolean areTokensExpired(){
+        return redisTemplate.opsForValue().get(CST_KEY) == null || redisTemplate.opsForValue().get(SECURITY_TOKEN_KEY) == null;
+    }
+}
